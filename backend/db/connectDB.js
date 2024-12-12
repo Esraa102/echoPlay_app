420974fa8a371f0c92c4418fd1f5275af516c7dd
Esraa102 <manalellban9@gmailcom>
2024-12-12 18:28:27 +0200

    connect to db

--- backend/db/connectDB.js
+++ backend/db/connectDB.js
@@ -2,7 +2,8 @@ import mongoose from "mongoose";
 
 const connectToDB = async () => {
   try {
-    await mongoose.connect();
+    await mongoose.connect(process.env.MONGODB_URI);
+    console.log("Connect to DB");
   } catch (error) {
     console.log(error);
     process.exit(1);
--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,12 @@
 import express from "express";
 import dotenv from "dotenv";
+import connectToDB from "./db/connectDB.js";
 dotenv.config();
 const app = express();
 
 const port = process.env.PORT || 3000;
+
+connectToDB();
 app.listen(port, () => {
   console.log(`Server started on http://localhost:${port}`);
 });
