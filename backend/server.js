cee22ba25263ec7493198a71b499d2a71660f33f
Esraa102 <manalellban9@gmailcom>
2024-12-12 18:40:18 +0200

    add built-in middleware

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,24 @@
 import express from "express";
 import dotenv from "dotenv";
-import connectToDB from "./db/connectDB.js";
 dotenv.config();
+import connectToDB from "./db/connectDB.js";
+import authRoute from "./routes/auth.route.js";
+import cookieParser from "cookie-parser";
+import cors from "cors";
 const app = express();
 
 const port = process.env.PORT || 3000;
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    methods: ["GET", "POST", "DELETE"],
+    credentials: true,
+  })
+);
+app.use("/api/v1/auth", authRoute);
 
 connectToDB();
 app.listen(port, () => {
